928df61cc26fc2e238a88874e1ab576ea6cbe9be
Ian Tjok <ianptjok@hotmail.com>
2025-05-04 18:50:08 -0700

    Added get api (:tripId/locations) for locations in TripRouter.ts
    Full path: /trip/:tripId/locations
    Returns the location collection for a trip

--- expressServer/routes/TripRouter.ts
+++ expressServer/routes/TripRouter.ts
@@ -1,15 +1,18 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { TripController } from '../controllers/TripController';
 import {TripModel} from '../model/TripModel';
+import {LocationModel} from '../model/LocationModel';
 
 class TripRouter {
   public router: Router;
   private tripController: TripController;
   public Trips:TripModel;
+  public Locations:LocationModel;
 
   constructor(mongoDBConnection:string) {
     this.tripController = new TripController();
     this.Trips = new TripModel(mongoDBConnection);
+    this.Locations = new LocationModel(mongoDBConnection);
 
     this.router = Router();
     this.routes();
@@ -22,11 +25,16 @@ class TripRouter {
     });
 
 
-    this.router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
-      var id = req.params.id;
+    this.router.get('/:tripId', async (req: Request, res: Response, next: NextFunction) => {
+      var id = req.params.tripId;
       await this.Trips.retrieveTrips(res, id);
     });
   
+    this.router.get('/:tripId/locations', async (req, res) => {
+      var id = req.params.tripId;
+      console.log('Query single trip with id: ' + id);
+      await this.Locations.retrieveLocationsDetails(res, {tripId: id});
+  });
   }
 
   
