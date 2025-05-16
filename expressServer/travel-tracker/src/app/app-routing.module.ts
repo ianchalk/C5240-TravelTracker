151b31f46da451dbead411c644e972ca5338003f
Jenitza-182 <jenitza.ruizc@gmail.com>
2025-05-16 16:21:17 -0700

    push test

--- expressServer/travel-tracker/src/app/app-routing.module.ts
+++ expressServer/travel-tracker/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomepageComponent } from './welcomepage/welcomepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomepageComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- expressServer/travel-tracker/src/app/app.module.ts
+++ expressServer/travel-tracker/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomepageComponent } from './welcomepage/welcomepage.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomepageComponent
+    WelcomepageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
