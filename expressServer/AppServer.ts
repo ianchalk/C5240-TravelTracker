9aa1ca65f17dfbc8c396b80677c0fd9550f7a1a6
Ian Tjok <ianptjok@hotmail.com>
2025-05-15 21:41:03 -0700

    Updated .env and AppServer.ts to connect to online MongoDB database

--- expressServer/AppServer.ts
+++ expressServer/AppServer.ts
@@ -6,9 +6,9 @@ dotenv.config();
 const port = process.env.PORT;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
-const mongoDBConnection = 'mongodb://' + dbUser + ':' + encodeURIComponent(dbPassword) + process.env.DB_INFO;
+const dbProtocol = process.env.DB_PROTOCOL;
+const mongoDBConnection = dbProtocol + dbUser + ':' + encodeURIComponent(dbPassword) + process.env.DB_INFO;
 console.log("server db connection URL " + mongoDBConnection);
-
 let server: any = new App(mongoDBConnection).express;
 server.listen(port);
 console.log("server running in port " + port);
