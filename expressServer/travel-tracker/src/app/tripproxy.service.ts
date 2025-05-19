364def8d913c3568b23c55d5dac22469c1fe0552
Ian Tjok <ianptjok@hotmail.com>
2025-05-19 15:48:56 -0700

    Added angular proxy service

---
+++ expressServer/travel-tracker/src/app/tripproxy.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripproxyService {
+
+  //hostUrl:string = 'https://traveltracker2025.azurewebsites.net/';
+  hostUrl:string = 'http://localhost:8080/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getListsIndex() {
+    return this.httpClient.get<any[]>( this.hostUrl + 'trip');
+  }
+
+  getItems(index: string) {
+    return this.httpClient.get( this.hostUrl + 'trip/' + index);
+  }
+}
