76edad547ec4b4c1807f852e57f315e535895717
Ian Tjok <ianptjok@hotmail.com>
2025-05-19 18:35:03 -0700

    Updated angular locations component/page

--- expressServer/traveltracker-application/src/app/locations/locations.component.ts
+++ expressServer/traveltracker-application/src/app/locations/locations.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { TripproxyService } from '../tripproxy.service';
+import { LocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-locations',
@@ -7,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrl: './locations.component.css'
 })
 export class LocationsComponent {
+ name: string = "";
+  tripId: string;
+  locationItems: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private list$: TripproxyService,
+    private location: LocationStrategy
+  ) { 
+    this.tripId = route.snapshot.params['id'];
+    const state = location.getState() as {name:string};
+    if (state != null) {
+      this.name = state.name;
+    } else {
+      this.name = "";
+    }
+    this.list$.getItems(this.tripId).subscribe((res: any) => {
+      this.locationItems = res;
+    });
+  }
+
+  ngOnInit():void {}
 
 }
