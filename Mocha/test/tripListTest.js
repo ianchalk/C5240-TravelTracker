eca2ba695277fcaf2c14e3eee7ca0076aa9c3ce3
Hongru He <hhe@seattleu.edu>
2025-05-20 17:11:14 -0700

    Create mocha tests

---
+++ Mocha/test/tripListTest.js
@@ -0,0 +1,57 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var async = require('async');
+
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should();
+
+var http = require('http');
+chai.use(chaiHttp);
+
+describe('Test trip lists result', function () {
+//	this.timeout(15000);
+
+	var requestResult;
+	var response;
+		 
+    before(function (done) {
+        chai.request("https://traveltracker2025.azurewebsites.net/")
+			.get("/trip")
+			.end(function (err, res) {
+				requestResult = res.body;
+				response = res;
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+				done();
+			});
+        });
+    
+    it('Should return an array object with more than 2 objects', function (){
+		expect(response).to.have.status(200);
+        expect(response.body).to.be.an('array');
+		expect(response.body).to.have.length.above(2);
+		expect(response).to.have.headers;
+    });
+    
+	it('The first entry in the array has known properties', function(){
+	    expect(requestResult[0]).to.include.keys('name');
+	    expect(requestResult[0]).to.have.property('_id');
+		expect(response.body[0]).to.have.deep.property('tripId');
+		expect(response.body).to.not.be.a.string;
+	});
+
+	it('The elements in the array have the expected properties', function(){
+		expect(response.body).to.have.length(3);
+		expect(response.body).to.satisfy(
+			function (body) {
+				for (var i = 0; i < body.length; i++) {
+					expect(body[i]).to.have.property('name');
+					expect(body[i]).to.have.property('description');
+					expect(body[i]).to.have.property('tripId');
+				}
+				return true;
+			});
+	});	
+	
+});
