5ea14a751a370368a275ebf802eb3ba8f959ca79
Ian Tjok <ianptjok@hotmail.com>
2025-06-09 23:45:06 -0700

    Update environment.prod.js
    replaced api key with a placeholder

--- travel-tracker/src/environments/environment.prod.js
+++ travel-tracker/src/environments/environment.prod.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.environment = void 0;
 exports.environment = {
     production: true,
-    googleMapsApiKey: 'AIzaSyASAPW3YNxF41QC75S7YT3EBFBwp54c0KA'
+    googleMapsApiKey: 'Update environment.prod.ts with placeholder key'
 };
 //# sourceMappingURL=environment.prod.js.map
